0aa22e3862952ac5a578ee014ea4aef208aed79d
Aminata Bangoura <foreverazo2013@gmail.com>
2019-11-08 01:00:47 +0100

    Initial Commit

---
+++ Project2GuessTheFlagSwiftUIKit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project2GuessTheFlagSwiftUIKit
+//
+//  Created by Aminata Bangoura on 08/11/2019.
+//  Copyright © 2019 Aminata Bangoura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
